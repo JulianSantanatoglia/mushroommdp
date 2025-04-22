5675338e1a53090ce5dd6a695d08316bb7c00a28
unknown <juliansantanatoglia@gmail.com>
2025-04-22 15:40:50 +0200

    fix: add terser for production build and update vite config

--- vite.config.js
+++ vite.config.js
@@ -20,6 +20,12 @@ export default defineConfig({
     },
     cssCodeSplit: true,
     minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+      },
+    },
   },
   resolve: {
     alias: {
